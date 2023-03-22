31534938c23bfe2d1e69ca1a7bbaa1ade736b61f
Volodymyr Hrudnytskyi <lintalve@gmail.com>
2023-03-22 18:41:47 +0100

    Initial commit.
    Tetris2 main class is created. A window (JFrame) is created, configured and added to Tetris2 class.
    GameArea class extended JPanel is created. Its basic qualitys configured to make it visible on the main window, that's it.

---
+++ src/GameArea.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GameArea extends JPanel {
+    public GameArea(){
+        this.setBounds(0, 0, 150, 150);
+        this.setBackground(Color.CYAN);
+
+    }
+}
---
+++ src/Tetris2.java
@@ -0,0 +1,37 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Tetris2 {
+    JFrame window;
+    GameArea ga;
+
+    public Tetris2(){
+        final int Width = 600;
+        final int Height = 1000;
+
+        window = new JFrame();
+        window.setBounds(400, 200, Width + 350, Height);  // setting dimentions for main window
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.ga = new GameArea();                                 //new GameArea
+        window.getContentPane().add(ga, BorderLayout.CENTER);     //is added to main window
+        window.getContentPane().setLayout(null);                  //enabling manual dimentions
+
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Tetris2 tetris2 = new Tetris2();
+                    tetris2.window.setVisible(true);
+                    //window.frame.pack();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+}
